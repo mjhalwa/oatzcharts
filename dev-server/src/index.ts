fe86a96b57fe301653a228bf7c7274937f218aa8
mjhalwa <mjhalwa.codes@gmail.com>
2021-09-15 21:54:44 +0200

    adds cors to dev server

--- dev-server/src/index.ts
+++ dev-server/src/index.ts
@@ -3,7 +3,18 @@ import { Request, Response, NextFunction } from 'express';
 import * as express from 'express';
 const app = express.default();
 
+// CORS
+// see
+// - https://stackoverflow.com/questions/50968152/cross-origin-request-blocked-with-react-and-express
+// - https://brianflove.com/2017-03-22/express-cors-typescript/
+// - https://www.twilio.com/blog/add-cors-support-express-typescript-api
+// the TypeScript way
+// ???
+// the JavaScript Way... works
+const cors = require('cors');
+app.use(cors())
 
+// data
 app.get('/rocketleague/api/all', function (req: Request, res: Response, next: NextFunction) {
   res.sendFile(path.join(__dirname + '/../all.json'));
 });
