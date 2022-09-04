6f2ec7766a815d21f72c3c82c1aaa2cf97bbbada
mjhalwa <mjhalwa.codes@gmail.com>
2022-09-04 21:55:39 +0200

    fixes wrong path in subtree push of deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -4,4 +4,4 @@ npm --prefix ./oatz-charts run build
 # add build to git and github
 git add ./oatz-charts/build && git commit -m "update with current build" && git push
 # override gh-pages branch with build directory content
-git subtree push --prefix ./oatz-charts/build origin gh-pages
+git subtree push --prefix oatz-charts/build origin gh-pages
