3dbcb20c44b85bb114b9aed5b13991b49c38e6c6
mjhalwa <mjhalwa.codes@gmail.com>
2022-09-04 22:57:29 +0200

    adds some console output to deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -18,9 +18,11 @@ elseif(git status --porcelain |Where {$_ -notmatch '^\?\?'}) {
 }
 else {
   # Git tree is clean
+  Write-Host "[Note] git tree is clean" -ForegroundColor "DarkCyan"
 
   # see https://gist.github.com/cobyism/4730490
   # build from current source
+  Write-Host "building oatz-charts..." -ForegroundColor "DarkCyan"
   npm --prefix ./oatz-charts run build
 
   # add build to git and github
@@ -28,8 +30,10 @@ else {
   #git add oatz-charts/build && git commit -m "update with current build" && git push
   # -> Note: -and is not a correct replacement for && operator
   # -> thus replacement of && with condition on $? to check previous command succeeded
+  Write-Host "adding build to git and GitHub..." -ForegroundColor "DarkCyan"
   git add ./oatz-charts/build; if ($?) { git commit -m "update with current build"; if ($?) { git push } }
 
   # override gh-pages branch with build directory content
+  Write-Host "push build directory content to gh-pages branch on GitHub..." -ForegroundColor "DarkCyan"
   git subtree push --prefix oatz-charts/build origin gh-pages
 }
